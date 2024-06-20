c6cdfd2e7c90b0e5e60c92497a39f5f7866b09eb
Natty A <natty.natty9402@gmail.com>
2024-06-20 17:04:45 +0300

    Tailwind not working | final commit just to make the day count

--- portfolio/app/components/Header.jsx
+++ portfolio/app/components/Header.jsx
@@ -3,9 +3,12 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <Link href={"/test"}>test</Link>
-    </div>
+    <header className="py-8 xl:py-12 text-white">
+      <div className="mx-auto">
+        <Link href={"/"}>hola</Link>
+        <h1 className="text-green-300">Test</h1>
+      </div>
+    </header>
   );
 };
 
